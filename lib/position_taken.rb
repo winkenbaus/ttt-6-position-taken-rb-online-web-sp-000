ecf796dd63c5f70fe89eb35a0bd62d350756c97a
caitlin <caitlin.wink@gmail.com>
2021-05-17 16:02:32 -0700

    commit tree f6be04587bbdaf9549fe0ae01f3bdc8d50a94d32

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+
+def position_taken?(board, index)
+    if board[index] == "X" || board[index] == "O"
+        true
+    else 
+        #board[index] == "" || board[index] == " "
+        false
+end 
+end
